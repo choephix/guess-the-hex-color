a27133ed5b9bea0b3177b30ecc88eccaa446496a
choephix <choephix@gmail.com>
2024-06-08 20:02:10 +0300

    --

---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'preact/hooks';
+import './app.css';
+
+export function App() {
+  const [colorCode, setColorCode] = useState('FFFFFF');
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const newColorCode = event.target?.elements?.colorInput.value;
+    console.log(newColorCode);
+    setColorCode(newColorCode);
+  };
+
+  return (
+    <div class='game-container'>
+      <h1>♥{colorCode}</h1>
+      <div class='color-square' style={{ backgroundColor: `#${colorCode}` }}></div>
+      <form onSubmit={handleSubmit} class='input-zone'>
+        <input type='text' name='colorInput' class='color-input' placeholder='Enter color code' />
+        <button type='submit' class='submit-button'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
