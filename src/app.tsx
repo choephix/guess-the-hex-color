c536eb80067e359bf171161b6f89080af39aeb82
choephix <choephix@gmail.com>
2024-06-08 20:14:38 +0300

    chore: Add input validation for color code input

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,13 @@ import './app.css';
 export function App() {
   const [colorCode, setColorCode] = useState('FFFFFF');
 
+  const handleInputChange = (event: any) => {
+    let value = event.target.value.toUpperCase();
+    value = value.slice(0, 6);
+    value = value.replace(/[^A-Fa-f0-9]/g, '');
+    event.target.value = value;
+  };
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
 
@@ -18,7 +25,7 @@ export function App() {
       <h1>♥{colorCode}</h1>
       <div class='color-square' style={{ backgroundColor: `#${colorCode}` }}></div>
       <form onSubmit={handleSubmit} class='input-zone'>
-        <input type='text' name='colorInput' class='color-input' placeholder='Enter color code' />
+        <input type='text' name='colorInput' class='color-input' placeholder='Enter color code' onInput={handleInputChange} />
         <button type='submit' class='submit-button'>
           Submit
         </button>
